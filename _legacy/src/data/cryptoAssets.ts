a1f4e7a75ebe9ca09c5eede493551531866b4966
Elliot Braem <elliot@ejlbraem.com>
2025-06-05 13:04:57 -0500

    delete legacy

--- _legacy/src/data/cryptoAssets.ts
+++
@@ -1,5 +0,0 @@
-// This file is kept for backward compatibility
-// It re-exports everything from the new structure
-import { assets, stablecoinSymbols } from "./assets";
-
-export { assets, stablecoinSymbols };
