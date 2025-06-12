b0fd5d2bf89db0f08f5ebcb94315d59d179810b9
Muhammad Saad Iqbal <saadiqbal.dev@outlook.com>
2025-06-12 17:54:59 +0500

    fix: popup style (#25)

--- popup/src/components/layout/popup-layout.tsx
+++ popup/src/components/layout/popup-layout.tsx
@@ -7,7 +7,7 @@ interface PopupLayoutProps {
 
 const PopupLayout: React.FC<PopupLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col items-center mx-auto my-auto md:min-h-screen justify-center text-white md:bg-white bg-[#121212]">
+    <div className="popup-root flex flex-col items-center mx-auto my-auto md:min-h-screen justify-center text-white md:bg-white bg-[#121212]">
       <div className="w-full flex p-4 min-h-screen md:min-h-auto flex-col justify-between rounded-[16px] h-full md:w-[500px] md:h-[700px] bg-[#121212]">
         <main className="flex-1 overflow-auto">{children}</main>
         <footer className="text-center text-xs text-white flex gap-1 items-end justify-center py-2">
