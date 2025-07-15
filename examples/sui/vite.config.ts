190a6af407b55e210d5fe55cfeef84c7454848ad
Elliot Braem <elliot@ejlbraem.com>
2025-07-15 17:57:01 -0500

    rearrange and clone

---
+++ examples/sui/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  define: {
+    "import.meta.env.POPUP_URL":
+      command === "serve"
+        ? JSON.stringify("http://localhost:5173")
+        : "undefined",
+  },
+  plugins: [],
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    exclude: ["@pingpay/onramp-sdk"],
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+}));
